8e0a51f926bb684c6687f9b340ac25ebbefdaa73
pedrolgr <pedrolgr47@gmail.com>
2024-05-07 19:24:30 -0300

    Adicionado DatePicker na criação da despesa e a forma que a data é visualizada na homepage

--- lib/scr/views/pages/cadastrar_despesa.dart
+++ lib/scr/views/pages/cadastrar_despesa.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CadastroPage extends StatefulWidget {
   const CadastroPage({Key? key}) : super(key: key);
@@ -15,6 +17,19 @@ class _CadastrarDispesa extends State<CadastroPage> {
   TextEditingController _nomeController = TextEditingController();
   TextEditingController _valorController = TextEditingController();
   TextEditingController _dataController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null)
+      setState(() {
+        _dataController.text = DateFormat('dd/MM/yyyy').format(picked);
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +57,10 @@ class _CadastrarDispesa extends State<CadastroPage> {
                 TextFormField(
                   controller: _dataController,
                   decoration: InputDecoration(labelText: 'Data'),
+                  readOnly: true,
+                  onTap: () {
+                    _selectDate(context);
+                  },
                 ),
                 ElevatedButton(
                   onPressed: () {
